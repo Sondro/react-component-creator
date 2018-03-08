88387fee3965a015f21ed832e1120d8b7d87ba7d
Gustavo Domaradzki <gustavo.domaradzki@ecocentauro.com.br>
2018-03-08 14:03:24 -0300

    Add stateless component

---
+++ templates/StatelessComponent/index.js
@@ -0,0 +1,16 @@
+const StatelessComponent =
+`import React from 'react'
+import PropTypes from 'prop-types'
+
+const [component] = props => (
+
+)
+
+[component].propTypes = {
+
+}
+
+export default [component]
+`
+
+module.exports = StatelessComponent
