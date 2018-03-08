308d8da304029cc6657ca6e3b70a679718d38c95
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-03-08 00:17:15 -0300

    Improve questions, add new questions, add directoryfinder

--- index.js
+++ index.js
@@ -6,17 +6,14 @@ const inquirer = require('./lib/inquirer')
 const directoryFinder = require('./lib/directoryFinder')
 
 clear()
-console.log(
-  chalk.cyan(
-    figlet.textSync('RCC', { horizontalLayout: 'full' })
-  )
-)
+console.log(chalk.cyan(figlet.textSync('RCC', { horizontalLayout: 'full' })))
+console.log(chalk.green('React Component Creator'))
 
 const run = async () => {
-  // const language = await inquirer.localeQuestionnaire()
-  // const answers = await inquirer.componentCreationQuestionnaire(language)
-  const directory = await directoryFinder.find('templates')
-  // console.log(directory)
+  const language = await inquirer.localeQuestionnaire()
+  const answers = await inquirer.componentCreationQuestionnaire(language)
+  const directory = await directoryFinder(answers.componentPath)
+  console.log(directory)
 }
 
 
