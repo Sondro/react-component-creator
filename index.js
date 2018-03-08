cdd3e1b5513ed4fe059b38d9fcd786ac304bdcd6
Gustavo Domaradzki <gustavo.domaradzki@ecocentauro.com.br>
2018-03-08 13:24:50 -0300

    Remove console.logs and import new modules

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const figlet = require('figlet')
 const files = require('./lib/files')
 const inquirer = require('./lib/inquirer')
 const directoryFinder = require('./lib/directoryFinder')
+const componentCreator = require('./lib/componentCreator')
 
 clear()
 console.log(chalk.cyan(figlet.textSync('RCC', { horizontalLayout: 'full' })))
@@ -12,7 +13,8 @@ console.log(chalk.green('React Component Creator'))
 const run = async () => {
   const language = await inquirer.localeQuestionnaire()
   const answers = await inquirer.componentCreationQuestionnaire(language)
-  const directory = await directoryFinder(answers.componentPath, language)
+  const directory = await directoryFinder.finder(answers.componentPath, language)
+  await componentCreator(answers.componentName, answers.componentPath, language)
 }
 
 run()
